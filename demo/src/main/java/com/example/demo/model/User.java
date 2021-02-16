bd40f7464c6bf99b85f97eaece20106a62dc1e8d
kevarey reynolds <kevareynolds@gmail.com>
2021-02-16 08:47:45 -0600

    first commit

---
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,16 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+}
